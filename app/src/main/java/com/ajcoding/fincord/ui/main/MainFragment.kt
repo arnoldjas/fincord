f224a41d75086a9f019e1b46811d319e06a38e97
arnoldjas <jasonhayadi036@gmail.com>
2022-04-05 17:23:58 +0700

    rename fragment dependencies

--- app/src/main/java/com/ajcoding/fincord/ui/main/MainFragment.kt
+++ app/src/main/java/com/ajcoding/fincord/ui/main/MainFragment.kt
@@ -5,19 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.ajcoding.fincord.databinding.MainFragmentBinding
+import com.ajcoding.fincord.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment: Fragment() {
-    private lateinit var binding: MainFragmentBinding
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = MainFragmentBinding.inflate(inflater, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
